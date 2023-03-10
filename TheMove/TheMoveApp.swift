f8205c9f7424bdcc0484f6cc09a74df3a8b3f95e
Jacquese <jacqwhitson@gmail.com>
2023-03-10 13:50:07 -0500

    Initial Commit

---
+++ TheMove/TheMoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMoveApp.swift
+//  TheMove
+//
+//  Created by Jacquese Whitson  on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
