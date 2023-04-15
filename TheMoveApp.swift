87b853d97517e3c5ebed54678acbc243abb2649d
Jacquese <jacqwhitson@gmail.com>
2023-04-15 19:46:42 -0400

    connected to Firebase

--- TheMoveApp.swift
+++ TheMoveApp.swift
@@ -6,13 +6,17 @@
 //
 
 import SwiftUI
-
+import Firebase
 @main
 struct TheMoveApp: App {
     @EnvironmentObject private var vm: EventsViewModel
     @EnvironmentObject private var cityVm: CitiesViewModel
-
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
+        
+        
         WindowGroup {
             HomePageView()
                 .environmentObject(CitiesViewModel())
